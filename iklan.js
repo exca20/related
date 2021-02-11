6c03ef5fe59d563a50a2574eec3bdb8f398b0d03
exca20 <78886276+exca20@users.noreply.github.com>
2021-02-11 11:07:35 +0700

    Add files via upload

---
+++ iklan.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '7610e674b302d2caa3c6c71646cd2a38',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.displaynetworkcontent.com/7610e674b302d2caa3c6c71646cd2a38/invoke.js"></scr' + 'ipt>');
